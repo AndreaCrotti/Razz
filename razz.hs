f33f586f3e1d394603c124830b30e7b0e23fc11b
Andrea Crotti <kerny404@gmail.com>
2010-01-25 00:40:30 +0100

    start ruby

--- razz.hs
+++ razz.hs
@@ -18,3 +18,18 @@ instance Show Card where
 mkDeck :: [Suit] -> [Value] -> Deck
 mkDeck sx vx = [ Card s v | s <- sx, v <- vx ]
 
+razzCards = "A" : map show [2..10] ++ ["J", "Q", "K"]
+razzSuits = ["X", "Y", "Z", "W"]
+razzDeck = mkDeck razzCards razzSuits
+
+getValue :: Value -> Int
+getValue v = case v of
+               "A" -> 1
+               "J" -> 11
+               "Q" -> 12
+               "K" -> 13
+               x -> (read x)::Int
+{-
+  Using immutable structures is not very good for perfomances here
+-}
+
--- razz.rb
+++ razz.rb
@@ -1,1 +1,24 @@
+#!/usr/bin/env ruby -w
 
+=begin rdoc
+  simulation for Razz game
+=end
+
+class Hand
+  
+end
+
+class Deck
+  def initialize(suits, ranks)
+    
+  end
+  
+  def to_s
+    
+  end
+
+end
+
+
+suit = "a".."d"
+rank = %w[A J Q K].concat((2..10).to_a
