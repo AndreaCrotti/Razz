76a2f865bee6db47d598c3bded1384d16e7dceee
Andrea Crotti <kerny404@gmail.com>
2010-01-28 13:36:47 +0100

    poker and flushes DO NOT COUNT, they're just discarded

--- razz.erl
+++ razz.erl
@@ -1,4 +1,12 @@
+%% Remember to use ~ instead of % for formatting issues
 -module(razz).
+
+-export([start/0]).
+
+start() ->
+    io:format("ciao~n").
+
+    
 
 
 
--- razz.py
+++ razz.py
@@ -4,11 +4,8 @@
 
 import unittest
 import random
-from itertools import product, izip
+from itertools import izip
 from sys import argv
-
-# TODO: remove the ugly global stuff
-# TODO: test everything better
 
 NROUNDS = 5
 MAXPLAYER = 8
@@ -26,13 +23,11 @@ class RazzGame(object):
 class Deck(object):
     def __init__(self, card_list):
         self.cards = {}
-        self.card_list = card_list
         for c in card_list:
-            # using setdefault?
-            if self.cards.has_key(c):
-                self.cards[c] += 1
-            else:
-                self.cards[c] = 1
+            self.addCard(c)
+
+    def __str__(self):
+        return ", ".join(self.cards.keys())
 
     def getCard(self, card):
         "Returns one card or delete the entry when they're finished"
@@ -50,43 +45,55 @@ class Deck(object):
 
     def addCard(self, card):
         if self.cards.has_key(card):
-            self.cards[card] = 1
-        else:
             self.cards[card] += 1
-                
+        else:
+            self.cards[card] = 1
 
 class RazzHand(Deck):
+    """
+    We don't take into account flush or straight
+    and simply discard anything that is not a high card
+    """
+    TOT_CARDS = 7
+    EVAL_CARDS = 5
     def __init__(self, card_list):
         super(RazzHand, self).__init__(card_list)
     
+    # works only when everything has been normalized already
     def __cmp__(self, other):
-        # here is the smart part, must check for poker full or pair
-        for s,o in izip(sorted(self.cards, reverse=True), sorted(other.cards, reverse=True)):
+        if self.has_duplicates():
+            if other.has_duplicates():
+                return 0
+            else:
+                return -1
+        if other.has_duplicates():
+            return 1
+
+        # getting the list of cards and sorting them at same time
+        cself, cother = sorted(self.cards.keys()), sorted(other.cards.keys())
+        for s, o in izip(cself, cother) :
             if s < o:
                 return 1
             if s > o:
                 return -1
+        # only reaches here if they're perfectly equal
         return 0
 
-    def bestHand(self):
-        "Returns the best hand, which is the worst in razz games"
-        pass
-    
-    def is_poker(self):
-        return 4 in self.cards.values()
+    # maybe better to not remove completely but just create another list
+    def normalize(self):
+        "Remove all the pairs, we are sure we're not removing too much thanks to has_duplicates"
+        for k in self.cards.keys():
+            while self.cards[k] > 1:
+                self.getCard(k)
 
-    def is_full(self):
-        v = self.cards.values()
-        return 3 in v and 2 in v
+    def has_duplicates(self):
+        "If I have 4 equal cards even removing 3 out of 7 I still get a pair"
+        dups = sum(self.cards[k] - 1 for k in self.cards.keys())
+        if dups > (self.TOT_CARDS - self.EVAL_CARDS + 1):
+            return True
+        else:
+            return False
 
-    def is_pair(self):
-        return 2 in self.cards.values()
-
-    def is_two_pair(self):
-        "At least two pairs"
-        return self.cards.values().count(2) >= 2
-
-    
 class Player(object):
     def __init__(self, name):
         self.name = name
@@ -101,7 +108,7 @@ class TestRazzTester(unittest.TestCase):
         h1 = [13, 10, 7, 2, 1]
         self.assertTrue(RazzHand(h) < RazzHand(h1))
 
-    def test_Pair(self):
+    def test_PairLosesAgainstHighCard(self):
         p1 = [13, 13, 7, 6, 2]
         p2 = [10, 7, 2, 1, 1]
         self.assertTrue(RazzHand(p1) < RazzHand(p2))
@@ -110,6 +117,7 @@ class TestRazzTester(unittest.TestCase):
         pok = [6, 6, 6, 6, 3]
         low = [8, 4, 3, 2, 1]
         self.assertTrue(RazzHand(pok) < RazzHand(low))
+
 
 class TestDeck(unittest.TestCase):
     def test_deckNotEmpty(self):
@@ -123,11 +131,10 @@ def str_to_card(s):
         return RAZZ_CARDS[s] # not handling exceptions here?
 
 if __name__ == '__main__':
-    unittest.main()
     # getting the arguments
+#    unittest.main()
     idx = 1
     nplayers = int(argv[1])
     my_cards = map(str_to_card, argv[2 : 5])
     other_cards = map(str_to_card, argv[5 : 5 + nplayers])
     print nplayers, my_cards, other_cards
-    
